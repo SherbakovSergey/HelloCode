371fe38f988d5ed6b37abfb88a626842b30bcdfc
Sergey <sergey51979@mail.ru>
2022-07-11 20:10:05 +0300

    Random

--- Example003/Program.cs
+++ Example003/Program.cs
@@ -3,3 +3,8 @@ int numberA = 3;
 int numberB = 5;
 int result  = numberA + numberB;
 Console.WriteLine(result);
+
+numberA = new Random().Next(1,10);
+numberB = new Random().Next(1,10);
+result  = numberA + numberB;
+Console.WriteLine(result);
